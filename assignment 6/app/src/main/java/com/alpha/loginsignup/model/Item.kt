b28e7ae07890287863d5e16a9cd8f60a62c2bf77
unknown <rufaroamujuru@gmail.com>
2025-01-19 23:51:53 +0100

    Flow for isLoggedIn and Loading state in RecipesFragment

--- assignment 6/app/src/main/java/com/alpha/loginsignup/model/Item.kt
+++
@@ -1,8 +0,0 @@
-package com.alpha.loginsignup.model
-
-data class Item(val id: Int, val imageRes: Int, val title: String, val description: String)
-
-enum class ItemAction {
-    CLICK,
-    BUTTON_CLICK
-}
